62f74f25cacb03e09376f82f95fb0fd75f2e5a1a
Sayuk <sayukbohdan@gmail.com>
2023-01-07 20:49:20 +0200

    dd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,5 +2,36 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(SimpleLightbox);
+const imageContainer = document.querySelector(".gallery");
+const imgMarkup = createImgGallery(galleryItems);
+imageContainer.insertAdjacentHTML("beforeend",imgMarkup );
+
+function createImgGallery(galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__item" href="${original}" data-lightbox="lbox" onclick="return false">
+            <img 
+            class="gallery__image"
+            loading="lazy"
+            src='${preview}'
+            data-source="${original}"
+            alt="${description}"
+            
+            >
+            </a>
+            </li>
+        `
+    }).join("");
+}
+
+
+var gallery = new SimpleLightbox('.gallery a',{captionsData: 'alt', captionDelay: 250, });
+
+    gallery.on('show.simplelightbox', function (){
+});
+
+
+console.log(galleryItems);
