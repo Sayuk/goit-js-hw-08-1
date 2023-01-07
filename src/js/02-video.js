7aaded4c505f0ea43d63cd535391a22758e4bba0
Sayuk <sayukbohdan@gmail.com>
2023-01-08 00:52:04 +0200

    a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,12 @@ import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', function (currentTime) {
-    const seconds = currentTime.seconds;
-    localStorage.setItem('videoplayer-current-time', JSON.stringify.seconds)
-})
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
