1d163eb66ef54095c16fb640890acc4794528040
Sayuk <sayukbohdan@gmail.com>
2023-01-08 00:45:48 +0200

    change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,12 @@
 import Player from '@vimeo/player';
-сonsole.log(Player);
+
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
+
+player.on('timeupdate', function (currentTime) {
+    const seconds = currentTime.seconds;
+    localStorage.setItem('videoplayer-current-time', JSON.stringify.seconds)
+})
