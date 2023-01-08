7967a2a1ef43dde0b14bff0198c3ef946afe428c
Sayuk <sayukbohdan@gmail.com>
2023-01-08 13:24:16 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,37 +3,53 @@ import Player from '@vimeo/player';
 var throttle = require('lodash.throttle');
 
 
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// // player.on('timeupdate', throttle(onPlay, 1000));
+
+// // function onPlay({ seconds }) {
+// //   localStorage.setItem('videoplayer-current-time', seconds);
+// // }
+
+// // player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+
+// player.on('timeupdate', function (currentTime) {
+//   const seconds = currentTime.seconds;
+//         localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+//     });
+
+
+//     const timeOn = JSON.parse(localStorage.getItem('videoplayer-current-time'))
+// let timeMove = timeOn.seconds;
+// player.setCurrentTime(timeMove).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
+   
+const CURRENT_TIME = "videoplayer-current-time";
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
-// player.on('timeupdate', throttle(onPlay, 1000));
-
-// function onPlay({ seconds }) {
-//   localStorage.setItem('videoplayer-current-time', seconds);
-// }
-
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-
-player.on('timeupdate', function (currentTime) {
-  const seconds = currentTime.seconds;
-        localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
-    });
-
-
-    const timeOn = JSON.parse(localStorage.getItem('videoplayer-current-time'))
-let timeMove = timeOn.seconds;
-player.setCurrentTime(timeMove).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+player.on('timeupdate', throttle(onPlay, 1000));
    
+function onPlay ({ seconds }) {
+    localStorage.setItem(CURRENT_TIME, seconds)
+}
+setCurrentTime()
+function setCurrentTime(){
+    if(!localStorage.getItem(CURRENT_TIME)){
+        return
+    }
+    player.setCurrentTime(localStorage.getItem(CURRENT_TIME))
+}
