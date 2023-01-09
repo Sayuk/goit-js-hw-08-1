67bf31fdad3d1365491ea5253f135f76987de30f
Sayuk <sayukbohdan@gmail.com>
2023-01-09 22:40:23 +0200

    S

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,21 +7,18 @@ console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 
 const markup = galleryItems
-  .map(({preview, original, description}) => {
-        return `<li class="gallery__item listStyle = 'none'">
-        <a class="gallery__item" href="${original}" data-lightbox="lbox" onclick="return false">
-            <img 
-            class="gallery__image"
-            loading="lazy"
-            src='${preview}'
-            data-source="${original}"
-            alt="${description}"
-            
-            >
-            </a>
-            </li>
-        `
-    }).join("");
+  .map(
+    ({ preview, description, original }) => `<li>
+    <a class="gallery__item" href="${original}">
+      <img
+      onclick="return false;
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a></li>`
+  )
+  .join('');
 
 gallery.insertAdjacentHTML('beforeend', markup);
 
@@ -35,36 +32,3 @@ new SimpleLightbox('ul.gallery a', {
   captionDelay: 250,
   captionPosition: 'bottom',
 });
-
-
-
-// const imageContainer = document.querySelector(".gallery");
-// const imgMarkup = createImgGallery(galleryItems);
-// imageContainer.insertAdjacentHTML("beforeend",imgMarkup );
-
-// function createImgGallery(galleryItems){
-//     return galleryItems
-//     .map(({preview, original, description}) => {
-//         return `<li class="gallery__item listStyle = 'none'">
-//         <a class="gallery__item" href="${original}" data-lightbox="lbox" onclick="return false">
-//             <img 
-//             class="gallery__image"
-//             loading="lazy"
-//             src='${preview}'
-//             data-source="${original}"
-//             alt="${description}"
-            
-//             >
-//             </a>
-//             </li>
-//         `
-//     }).join("");
-// }
-
-
-
-
- 
-
-
-// console.log(galleryItems);
