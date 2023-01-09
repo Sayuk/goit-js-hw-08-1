d0d491755f01863a9c7502c6e96ee9127282f5bc
Sayuk <sayukbohdan@gmail.com>
2023-01-09 22:33:44 +0200

    a

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,17 +7,21 @@ console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 
 const markup = galleryItems
-  .map(
-    ({ preview, description, original }) => `<li>
-    <a class="gallery__item" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        alt="${description}"
-      />
-    </a></li>`
-  )
-  .join('');
+  .map(({preview, original, description}) => {
+        return `<li class="gallery__item listStyle = 'none'">
+        <a class="gallery__item" href="${original}" data-lightbox="lbox" onclick="return false">
+            <img 
+            class="gallery__image"
+            loading="lazy"
+            src='${preview}'
+            data-source="${original}"
+            alt="${description}"
+            
+            >
+            </a>
+            </li>
+        `
+    }).join("");
 
 gallery.insertAdjacentHTML('beforeend', markup);
 
@@ -31,3 +35,36 @@ new SimpleLightbox('ul.gallery a', {
   captionDelay: 250,
   captionPosition: 'bottom',
 });
+
+
+
+// const imageContainer = document.querySelector(".gallery");
+// const imgMarkup = createImgGallery(galleryItems);
+// imageContainer.insertAdjacentHTML("beforeend",imgMarkup );
+
+// function createImgGallery(galleryItems){
+//     return galleryItems
+//     .map(({preview, original, description}) => {
+//         return `<li class="gallery__item listStyle = 'none'">
+//         <a class="gallery__item" href="${original}" data-lightbox="lbox" onclick="return false">
+//             <img 
+//             class="gallery__image"
+//             loading="lazy"
+//             src='${preview}'
+//             data-source="${original}"
+//             alt="${description}"
+            
+//             >
+//             </a>
+//             </li>
+//         `
+//     }).join("");
+// }
+
+
+
+
+ 
+
+
+// console.log(galleryItems);
