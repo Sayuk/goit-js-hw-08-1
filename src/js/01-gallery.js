9b7327b0e540a44026c59e861c7cbc5d10f15866
Sayuk <sayukbohdan@gmail.com>
2023-01-09 23:27:03 +0200

    S

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,41 +1,33 @@
-// Описаний в документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
-
-const imageContainer = document.querySelector(".gallery");
-const imgMarkup = createImgGallery(galleryItems);
-imageContainer.insertAdjacentHTML("beforeend",imgMarkup );
-
-function createImgGallery(galleryItems){
-    return galleryItems
-    .map(({preview, original, description}) => {
-        return `<li class="gallery__item" listStyle = "none">
-        <a class="gallery__item" href="${original}" data-lightbox="lbox" >
-            <img 
-            
-            class="gallery__image"
-            loading="lazy"
-            src='${preview}'
-            data-source="${original}"
-            alt="${description}"
-            
-            >
-            </a>
-            </li>
-        `
-    }).join("");
-}
-
-
-new SimpleLightbox('.gallery a',{captionsData: 'alt', captionDelay: 250, });
-
- 
-
-
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, description, original }) => `<li>
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a></li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+const arrLinks = document.querySelectorAll('a');
+arrLinks.forEach(link => {
+  link.style.boxShadow = 'none';
+});
+
+new SimpleLightbox('ul.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
