4e05ba085f2ad49794d78af881b6617914edf382
Sayuk <sayukbohdan@gmail.com>
2023-01-09 22:54:36 +0200

    f

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,37 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
 import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from '../simplelightbox';
+import "../node_modules/simplelightbox/dist/simple-lightbox.min.css";
+
+const imageContainer = document.querySelector(".gallery");
+const imgMarkup = createImgGallery(galleryItems);
+imageContainer.insertAdjacentHTML("beforeend",imgMarkup );
+
+function createImgGallery(galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<li class="gallery__item listStyle = 'none'">
+        <a class="gallery__item" href="${original}" data-lightbox="lbox" >
+            <img 
+            onclick="return false;
+            class="gallery__image"
+            loading="lazy"
+            src='${preview}'
+            data-source="${original}"
+            alt="${description}"
+            
+            >
+            </a>
+            </li>
+        `
+    }).join("");
+}
+
+
+var gallery = new SimpleLightbox('.gallery a',{captionsData: 'alt', captionDelay: 250, });
+
+ 
+
 
 console.log(galleryItems);
-
-const gallery = document.querySelector('.gallery');
-
-const markup = galleryItems
-  .map(
-    ({ preview, description, original }) => `<li>
-    <a class="gallery__item" href="${original}">
-      <img
-      onclick="return false"
-        class="gallery__image"
-        src="${preview}"
-        alt="${description}"
-      />
-    </a></li>`
-  )
-  .join('');
-
-gallery.insertAdjacentHTML('beforeend', markup);
-
-const arrLinks = document.querySelectorAll('a');
-arrLinks.forEach(link => {
-  link.style.boxShadow = 'none';
-});
-
-new SimpleLightbox('ul.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  captionPosition: 'bottom',
-});
