070fcc0116f700098d23022ccea33e529773054d
Sayuk <sayukbohdan@gmail.com>
2023-01-09 23:56:09 +0200

    S

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,37 +1,44 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+// Add imports above this line
 import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
+const imageContainer = document.querySelector(".gallery");
+const imgMarkup = createImgGallery(galleryItems);
+imageContainer.insertAdjacentHTML("beforeend",imgMarkup );
 
-const markup = galleryItems
-  .map(
-    ({ preview, description, original }) => `<li>
-    <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
-      <img
-      
-        class="gallery__image"
-        src="${preview}"
-        alt="${description}"
-      />
-    </a></li>`
-      
-)
-    
-  .join('');
-
-gallery.insertAdjacentHTML('beforeend', markup);
-
-// const arrLinks = document.querySelectorAll('a');
-// arrLinks.forEach(link => {
-//     link.style.boxShadow = 'none';
-    
-// });
-
-new SimpleLightbox('ul.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  captionPosition: 'bottom',
+function createImgGallery(galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<li class="gallery__item" listStyle = "none">
+        <a class="gallery__item" href="${original}" data-lightbox="lbox"  >
+            <img 
+            
+            class="gallery__image"
+            loading="lazy"
+            src='${preview}'
+            data-source="${original}"
+            alt="${description}"
+            
+            >
+            </a>
+            </li>
+        `
+    }).join("");
+}
+const arrLinks = document.querySelectorAll('a');
+arrLinks.forEach(link => {
+  link.style.boxShadow = 'none';
 });
+
+new SimpleLightbox('.gallery a',{captionsData: 'alt', captionDelay: 250, });
+
+ 
+
+
