44a4739b706bd0977da4e0cb153e7909deaf5edd
Sayuk <sayukbohdan@gmail.com>
2023-01-09 23:18:12 +0200

    D

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -18,7 +18,7 @@ function createImgGallery(galleryItems){
         return `<li class="gallery__item listStyle = 'none'>
         <a class="gallery__item" href="${original}" data-lightbox="lbox" >
             <img 
-            onclick="return false"
+            
             class="gallery__image"
             loading="lazy"
             src='${preview}'
