030631c48a6a7223aa9628b10127dd053d24e6fc
Sayuk <sayukbohdan@gmail.com>
2023-01-13 00:54:57 +0200

    w

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,4 @@
 
-// import throttle from 'lodash.throttle';
-
-// const form = document.querySelector('.feedback-form');
-// form.addEventListener('input', throttle(onFormData, 500));
-// form.addEventListener('submit', onSubmitForm);
-
-// const date = {};
-// const STORAGE = 'feedback-form-state';
-
-// function onFormData(e) {
-//   date[e.target.name] = e.target.value;
-//   localStorage.setItem(STORAGE, JSON.stringify(date));
-
-  
-// }
-
-// function onSubmitForm(e) {
-//    e.preventDefault();
-//   e.currentTarget.reset();
-//   localStorage.removeItem(STORAGE);
-//   console.log(date)
-//   delete date.email;
-//   delete date.message;
-  
-// }
-  
-
-
-// function dataFromLocalStorage() {
-//   const date = JSON.parse(localStorage.getItem(STORAGE));
-//   const email = document.querySelector('.feedback-form input');
-//   const message = document.querySelector('.feedback-form textarea');
-//   if (date) {
-//     email.value = date.email || '';
-//     message.value = date.message || '';
-//   }
-// };
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
@@ -63,7 +26,7 @@ delete date.message;
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
   if (data) {
-    email.value = data.email;
-    message.value = data.message;
+   email.value = date.email || '';
+    message.value = date.message || '';
   }
 })();
