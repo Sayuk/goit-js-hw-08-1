c857a634621f2d7594863f145e73ad2598889288
Sayuk <sayukbohdan@gmail.com>
2023-01-13 00:58:50 +0200

    s

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,28 +5,65 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
-const formData = {};
+const date = {};
+const STORAGE = 'feedback-form-state';
 
 function onFormData(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  date[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE, JSON.stringify(date));
+
+  
 }
 
 function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  e.preventDefault();
+   e.preventDefault();
   e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(STORAGE);
+  console.log(date)
   delete date.email;
-delete date.message;
+  delete date.message;
+  
 }
+  
 
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+function dataFromLocalStorage() {
+  const date = JSON.parse(localStorage.getItem(STORAGE));
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-   email.value = date.email || '';
+  if (date) {
+    email.value = date.email || '';
     message.value = date.message || '';
   }
-})();
+};
+// import throttle from 'lodash.throttle';
+
+// const form = document.querySelector('.feedback-form');
+// form.addEventListener('input', throttle(onFormData, 500));
+// form.addEventListener('submit', onSubmitForm);
+
+// const formData = {};
+
+// function onFormData(e) {
+//   formData[e.target.name] = e.target.value;
+//   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+// }
+
+// function onSubmitForm(e) {
+//   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+//   e.preventDefault();
+//   e.currentTarget.reset();
+//   localStorage.removeItem('feedback-form-state');
+//   delete date.email;
+// delete date.message;
+// }
+
+// (function dataFromLocalStorage() {
+//   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+//   const email = document.querySelector('.feedback-form input');
+//   const message = document.querySelector('.feedback-form textarea');
+//   if (data) {
+//    email.value = date.email || '';
+//     message.value = date.message || '';
+//   }
+// })();
