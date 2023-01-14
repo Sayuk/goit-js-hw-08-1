df7a43551848a1314862ba2537843f1c6f6a31f6
Sayuk <sayukbohdan@gmail.com>
2023-01-14 23:12:09 +0200

    k

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,26 +6,64 @@ form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
 const formData = {};
+const STORAGE = 'feedback-form-state';
 
 function onFormData(e) {
   formData[e.target.name] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE, JSON.stringify(formData));
+
+  
 }
 
 function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  e.preventDefault();
+   e.preventDefault();
   e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-
+  localStorage.removeItem(STORAGE);
+  console.log(date)
+  delete date.email;
+  delete date.message;
+  
 }
+  
 
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+function dataFromLocalStorage() {
+  const date = JSON.parse(localStorage.getItem(STORAGE));
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-   email.value = date.email;
-    message.value = date.message;
+  if (date) {
+    email.value = date.email || '';
+    message.value = date.message || ''; 
   }
-})();
+};
+// import throttle from 'lodash.throttle';
+
+// const form = document.querySelector('.feedback-form');
+// form.addEventListener('input', throttle(onFormData, 500));
+// form.addEventListener('submit', onSubmitForm);
+
+// const formData = {};
+
+// function onFormData(e) {
+//   formData[e.target.name] = e.target.value;
+//   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+// }
+
+// function onSubmitForm(e) {
+//   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+//   e.preventDefault();
+//   e.currentTarget.reset();
+//   localStorage.removeItem('feedback-form-state');
+//   delete date.email;
+// delete date.message;
+// }
+
+// (function dataFromLocalStorage() {
+//   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+//   const email = document.querySelector('.feedback-form input');
+//   const message = document.querySelector('.feedback-form textarea');
+//   if (data) {
+//    email.value = date.email || '';
+//     message.value = date.message || '';
+//   }
+// })();
