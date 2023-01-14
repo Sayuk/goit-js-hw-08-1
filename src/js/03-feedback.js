84c7af521df40936424d1873a9e8e2fe4af73ca9
Sayuk <sayukbohdan@gmail.com>
2023-01-14 23:25:36 +0200

    end

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,4 @@
 
-// import throttle from 'lodash.throttle';
-
-// const form = document.querySelector('.feedback-form');
-// form.addEventListener('input', throttle(onFormData, 500));
-// form.addEventListener('submit', onSubmitForm);
-
-// const formData = {};
-// const STORAGE = 'feedback-form-state';
-
-// function onFormData(e) {
-//   formData[e.target.name] = e.target.value;
-//   localStorage.setItem(STORAGE, JSON.stringify(formData));
-
-  
-// }
-
-// function onSubmitForm(e) {
-//    e.preventDefault();
-//   e.currentTarget.reset();
-//   localStorage.removeItem(STORAGE);
-//   console.log(date)
-//   delete date.email;
-//   delete date.message;
-  
-// }
-  
-
-
-// function dataFromLocalStorage() {
-//   const date = JSON.parse(localStorage.getItem(STORAGE));
-//   const email = document.querySelector('.feedback-form input');
-//   const message = document.querySelector('.feedback-form textarea');
-//   if (date) {
-//     email.value = date.email || '';
-//     message.value = date.message || ''; 
-//   }
-// };
-
-
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form')
